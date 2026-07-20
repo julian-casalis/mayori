4fd11985b4368679bb04deea91bb08c2ebab3ac4
julian-casalis <escjuliancasalis@gmail.com>
2026-07-20 20:04:56 -0300

    Descripción de los cambios

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://julian-casalis.github.io',
+  base: '/mayori',
+  output: 'static'
+});
